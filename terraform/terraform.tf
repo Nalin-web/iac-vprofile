41b266971f8cc13100747a27c154456d57decccf
Nalin-web <nalinmudgal99@gmail.com>
2025-11-18 21:19:29 +0530

    jai jai

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -29,7 +29,7 @@ terraform {
   backend "s3" {
     bucket = "gitops-today"
     key    = "terraform.tfstate"
-    region = "ap-south-a"
+    region = "ap-south-1a"
   }
 
   required_version = "~> 1.6.3"
