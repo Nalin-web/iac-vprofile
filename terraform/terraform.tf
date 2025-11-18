3a24e8348e1c8b48b301e66f387e081d1220be2d
Nalin-web <nalinmudgal99@gmail.com>
2025-11-18 10:55:52 +0530

    Bucket name & cluster name updated

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "gitops-today"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "ap-south-a"
   }
 
   required_version = "~> 1.6.3"
